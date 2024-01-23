6b47ad52e936bb16ff3b0f456af87502bf977167
tuyenntph29022 <tuyenntph29022@fpt.edu.vn>
2024-01-23 16:54:09 +0700

    timeline hóa đơn - fix cứng

---
+++ src/main/java/com/poly/BeeShoes/service/LichSuHoaDonService.java
@@ -0,0 +1,11 @@
+package com.poly.BeeShoes.service;
+
+import com.poly.BeeShoes.model.LichSuHoaDon;
+
+import java.util.List;
+
+public interface LichSuHoaDonService {
+
+    List<LichSuHoaDon> getAllLichSuHoaDonByIdHoaDon(Long id);
+
+}
