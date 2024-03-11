b16bfb64e0b18934d2cc8108495756c069dddf51
anhtuan2212 <lutan2212@gmail.com>
2024-03-11 13:17:37 +0700

    fixx checkout

--- src/main/resources/static/assets/customer/js/checkout/checkout.js
+++ src/main/resources/static/assets/customer/js/checkout/checkout.js
@@ -1,4 +1,128 @@
 setTabsHeader('shop');
+var provinceArr = [];
+var districtArr = [];
+var wardArr = [];
+fetch('/assets/address-json/province.json')
+    .then(response => response.json())
+    .then(data => {
+        provinceArr = data;
+        return fillAllTinh(data);
+    })
+    .then(() => {
+        return fetch('/assets/address-json/district.json')
+            .then(response => response.json())
+            .then(data => {
+                districtArr = data;
+            });
+    })
+    .then(() => {
+        return fetch('/assets/address-json/ward.json')
+            .then(response => response.json())
+            .then(data => {
+                wardArr = data;
+                let defaultCheckedValue = $(".selected_product:checked").closest('.customerAddress');
+                if (defaultCheckedValue != null) {
+                    callApiShippingFee();
+                }
+            });
+    })
+    .catch(error => console.error('Error:', error));
+
+function fillAllTinh(data) {
+    let tinhThanhPho = $('#tinhThanhPho');
+    $.each(data, function (index, item) {
+        let html = `<option value="${item.ProvinceID}">${String(item.ProvinceName)}</option>`;
+        tinhThanhPho.append(html);
+    });
+}
+function callApiShippingFee() {
+    let defaultCheckedValue = $(".selected_product:checked").closest('.customerAddress');
+    let defaultHouseNumber = defaultCheckedValue.find('.customerHouseNumber').text().replace(/[,.]/g, '');
+    let defaultCustomerWardName = defaultCheckedValue.find('.customerWard').text().replace(/[,.]/g, '');
+    let defaultCustomerDistrictName = defaultCheckedValue.find('.customerDistrict').text().replace(/[,.]/g, '');
+    let defaultCustomerProvinceName = defaultCheckedValue.find('.customerProvince').text().replace(/[,.]/g, '');
+    let defaultWard, defaultDistrict, defaultProvince;
+    console.log(defaultCustomerWardName + ',' + defaultCustomerDistrictName + ',' + defaultCustomerProvinceName);
+    console.log(wardArr);
+    console.log(districtArr);
+    console.log(provinceArr)
+    provinceArr.forEach((item) => {
+        if (item.ProvinceName == defaultCustomerProvinceName) {
+            console.log(item.ProvinceName)
+            defaultProvince = item.ProvinceID;
+        }
+    })
+    districtArr.forEach((item) => {
+        if (item.ProvinceID == defaultProvince && item.DistrictName == defaultCustomerDistrictName) {
+            console.log(item.DistrictName)
+            defaultDistrict = item.DistrictID;
+        }
+    })
+    wardArr.forEach((item) => {
+        if (item.DistrictID = defaultDistrict && item.Name == defaultCustomerWardName) {
+            console.log(item.Name)
+            defaultWard = item.Code;
+        }
+    })
+    console.log(defaultWard + ',' + defaultDistrict + ',' + defaultProvince);
+
+    // $.ajax({
+    //     type: "POST",
+    //     url: "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/create",
+    //     contentType: "application/json",
+    //     headers: {
+    //         "Token": "68b8b44f-a88d-11ee-8bfa-8a2dda8ec551"
+    //     },
+    //     data: JSON.stringify(
+    //         {
+    //             shop_id: "190713",
+    //             from_name: "LightBee Shop",
+    //             from_phone: "0359966461",
+    //             from_address: "Trường Cao Đẳng FPT Polytechnic",
+    //             from_ward_name: "Phường Xuân Phương",
+    //             from_district_name: "Nam Từ Liêm",
+    //             from_province_name: "Hà Nội",
+    //             to_name: "test",
+    //             to_phone: "0359966461",
+    //             to_address: "Nam Đinh",
+    //             to_ward_code: defaultWard,
+    //             to_district_id: defaultDistrict,
+    //             service_id: 55320,
+    //             service_type_id: 2,
+    //             payment_type_id: 2,
+    //             cod_amount: parseInt(200000),
+    //             required_note: "CHOXEMHANGKHONGTHU",
+    //             items: [
+    //                 {
+    //                     name:"Áo Polo",
+    //                     code:"Polo123",
+    //                     quantity: 1,
+    //                     price: 200000,
+    //                     length: 12,
+    //                     width: 12,
+    //                     height: 12,
+    //                     weight: 1200,
+    //                     category:
+    //                         {
+    //                             level1:"Áo"
+    //                         }
+    //                 }
+    //             ],
+    //             weight: 2000,
+    //             length: 1,
+    //             width: 19,
+    //             height: 10
+    //         }
+    //     ),
+    //     success: function (response) {
+    //         $('#shippingFee').text(parseFloat(response.data.total_fee).toLocaleString('en-US'));
+    //         $('#totalAmount').text(parseFloat(parseInt(total) + parseInt(response.data.total_fee)).toLocaleString('en-US'));
+    //     },
+    //     error: function (error) {
+    //         console.error('Xảy ra lỗi: ', error)
+    //     }
+    // })
+}
 
 $(document).ready(function () {
 
@@ -8,62 +132,59 @@ $(document).ready(function () {
     var tinhThanhPho = $('#tinhThanhPho');
     var quanHuyen = $('#quanHuyen');
     var phuongXa = $('#phuongXa');
-    var provinceArr = [];
-    var districtArr = [];
-    var wardArr = [];
     var ward;
     var district;
     var province;
     var houseNumber;
 
-    $.ajax({
-        type: "GET",
-        url: "/assets/address-json/province.json",
-        contentType: "application/json",
-        success: function (response) {
-            provinceArr = response;
-            console.log(response)
-            console.log(123)
-            $.each(response, function (index, item) {
-                let html = `<option value="${item.ProvinceID}">${String(item.ProvinceName)}</option>`;
-                tinhThanhPho.append(html);
-            });
-        },
-        error: function (error) {
-            console.error('Xảy ra lỗi: ', error);
-        }
-    });
-
-    $.ajax({
-        type: "GET",
-        url: "/assets/address-json/district.json",
-        contentType: "application/json",
-        success: function (response) {
-            districtArr = response;
-        },
-        error: function (error) {
-            console.error('Xảy ra lỗi: ', error);
-        }
-    });
-
-    $.ajax({
-        type: "GET",
-        url: "/assets/address-json/ward.json",
-        contentType: "application/json",
-        success: function (response) {
-            wardArr = response;
-        },
-        error: function (error) {
-            console.error('Xảy ra lỗi: ', error);
-        }
-    })
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/assets/address-json/province.json",
+    //     contentType: "application/json",
+    //     success: function (response) {
+    //         provinceArr = response;
+    //         console.log(response)
+    //         console.log(123)
+    //         $.each(response, function (index, item) {
+    //             let html = `<option value="${item.ProvinceID}">${String(item.ProvinceName)}</option>`;
+    //             tinhThanhPho.append(html);
+    //         });
+    //     },
+    //     error: function (error) {
+    //         console.error('Xảy ra lỗi: ', error);
+    //     }
+    // });
+    //
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/assets/address-json/district.json",
+    //     contentType: "application/json",
+    //     success: function (response) {
+    //         districtArr = response;
+    //     },
+    //     error: function (error) {
+    //         console.error('Xảy ra lỗi: ', error);
+    //     }
+    // });
+    //
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/assets/address-json/ward.json",
+    //     contentType: "application/json",
+    //     success: function (response) {
+    //         wardArr = response;
+    //     },
+    //     error: function (error) {
+    //         console.error('Xảy ra lỗi: ', error);
+    //     }
+    // })
 
     tinhThanhPho.on('change', function () {
         tinhThanhPhoSelected = tinhThanhPho.val();
         province = tinhThanhPho.find("option:selected").text();
         quanHuyen.html('<option value="">Quận/Huyện</option>');
         phuongXa.html('<option value="">Phường/Xã</option>');
-        if(tinhThanhPhoSelected) {
+        if (tinhThanhPhoSelected) {
             districtArr.forEach(function (item) {
                 if (item.ProvinceID == tinhThanhPhoSelected) {
                     let html = `<option value="${item.DistrictID}">${String(item.DistrictName)}</option>`;
@@ -78,7 +199,7 @@ $(document).ready(function () {
         district = quanHuyen.find("option:selected").text();
         phuongXa.html('<option value="">Phường/Xã</option>');
         wardArr.forEach((item) => {
-            if(item.DistrictID == quanHuyenSelected) {
+            if (item.DistrictID == quanHuyenSelected) {
                 let html = `<option value="${item.Code}">${String(item.Name)}</option>`;
                 phuongXa.append(html);
             }
@@ -117,8 +238,8 @@ $(document).ready(function () {
                     required_note: "CHOXEMHANGKHONGTHU",
                     items: [
                         {
-                            name:"Áo Polo",
-                            code:"Polo123",
+                            name: "Áo Polo",
+                            code: "Polo123",
                             quantity: 1,
                             price: 200000,
                             length: 12,
@@ -127,7 +248,7 @@ $(document).ready(function () {
                             weight: 1200,
                             category:
                                 {
-                                    level1:"Áo"
+                                    level1: "Áo"
                                 }
                         }
                     ],
@@ -147,100 +268,6 @@ $(document).ready(function () {
         })
     })
 
-    var defaultCheckedValue = $(".selected_product:checked").closest('.customerAddress');
-    if(defaultCheckedValue != null) {
-        callApiShippingFee();
-    }
-
-    function callApiShippingFee() {
-        var defaultHouseNumber = defaultCheckedValue.find('.customerHouseNumber').text().replace(/[,.]/g, '');
-        var defaultCustomerWardName = defaultCheckedValue.find('.customerWard').text().replace(/[,.]/g, '');
-        var defaultCustomerDistrictName = defaultCheckedValue.find('.customerDistrict').text().replace(/[,.]/g, '');
-        var defaultCustomerProvinceName = defaultCheckedValue.find('.customerProvince').text().replace(/[,.]/g, '');
-        var defaultWard, defaultDistrict, defaultProvince;
-        console.log(defaultCustomerWardName + ',' + defaultCustomerDistrictName + ',' + defaultCustomerProvinceName);
-        console.log(wardArr);
-        console.log(districtArr);
-        console.log(provinceArr)
-        wardArr.forEach((item) => {
-            if(item.Name == defaultCustomerWardName) {
-                console.log(item.Name)
-                defaultWard = item.Code;
-            }
-        })
-        districtArr.forEach((item) => {
-            if(item.DistrictName == defaultCustomerDistrictName) {
-                console.log(item.DistrictName)
-                defaultDistrict = item.DistrictID;
-            }
-        })
-        provinceArr.forEach((item) => {
-            if(item.ProvinceName == defaultCustomerProvinceName) {
-                console.log(item.ProvinceName)
-                defaultProvince = item.ProvinceID;
-            }
-        })
-
-        console.log(defaultWard + ',' + defaultDistrict + ',' + defaultProvince);
-
-        // $.ajax({
-        //     type: "POST",
-        //     url: "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/create",
-        //     contentType: "application/json",
-        //     headers: {
-        //         "Token": "68b8b44f-a88d-11ee-8bfa-8a2dda8ec551"
-        //     },
-        //     data: JSON.stringify(
-        //         {
-        //             shop_id: "190713",
-        //             from_name: "LightBee Shop",
-        //             from_phone: "0359966461",
-        //             from_address: "Trường Cao Đẳng FPT Polytechnic",
-        //             from_ward_name: "Phường Xuân Phương",
-        //             from_district_name: "Nam Từ Liêm",
-        //             from_province_name: "Hà Nội",
-        //             to_name: "test",
-        //             to_phone: "0359966461",
-        //             to_address: "Nam Đinh",
-        //             to_ward_code: defaultWard,
-        //             to_district_id: defaultDistrict,
-        //             service_id: 55320,
-        //             service_type_id: 2,
-        //             payment_type_id: 2,
-        //             cod_amount: parseInt(200000),
-        //             required_note: "CHOXEMHANGKHONGTHU",
-        //             items: [
-        //                 {
-        //                     name:"Áo Polo",
-        //                     code:"Polo123",
-        //                     quantity: 1,
-        //                     price: 200000,
-        //                     length: 12,
-        //                     width: 12,
-        //                     height: 12,
-        //                     weight: 1200,
-        //                     category:
-        //                         {
-        //                             level1:"Áo"
-        //                         }
-        //                 }
-        //             ],
-        //             weight: 2000,
-        //             length: 1,
-        //             width: 19,
-        //             height: 10
-        //         }
-        //     ),
-        //     success: function (response) {
-        //         $('#shippingFee').text(parseFloat(response.data.total_fee).toLocaleString('en-US'));
-        //         $('#totalAmount').text(parseFloat(parseInt(total) + parseInt(response.data.total_fee)).toLocaleString('en-US'));
-        //     },
-        //     error: function (error) {
-        //         console.error('Xảy ra lỗi: ', error)
-        //     }
-        // })
-    }
-
     $('.selected_product').on('change', function () {
         var customerAddress = $(this).closest('.customerAddress')
         houseNumber = customerAddress.find('.customerHouseNumber').text().replace(/[,.]/g, '');
@@ -248,17 +275,17 @@ $(document).ready(function () {
         var customerDistrictName = customerAddress.find('.customerDistrict').text().replace(/[,.]/g, '');
         var customerProvinceName = customerAddress.find('.customerProvince').text().replace(/[,.]/g, '');
         wardArr.forEach((item) => {
-            if(item.Name == customerWardName) {
+            if (item.Name == customerWardName) {
                 ward = item.Code;
             }
         })
         districtArr.forEach((item) => {
-            if(item.DistrictName == customerDistrictName) {
+            if (item.DistrictName == customerDistrictName) {
                 district = item.DistrictID;
             }
         })
         provinceArr.forEach((item) => {
-            if(item.ProvinceName == customerProvinceName) {
+            if (item.ProvinceName == customerProvinceName) {
                 province = item.ProvinceID;
             }
         })
@@ -267,7 +294,7 @@ $(document).ready(function () {
     $('#placeOrder').on('click', function () {
         var total = parseInt($('#total').text().replace(/[,.]/g, ''));
         var shippingFee = parseInt($('#shippingFee').text().replace(/[,.]/g, ''));
-        if($('#customerHouseNumber').val() == null || $('#customerHouseNumber').val() === undefined) {
+        if ($('#customerHouseNumber').val() == null || $('#customerHouseNumber').val() === undefined) {
             houseNumber = $('#customerHouseNumber').val();
         }
         console.log(houseNumber + ',' + ward + ',' + district + ',' + province);
@@ -277,7 +304,7 @@ $(document).ready(function () {
         console.log(typeOfPayment);
         var voucherCode = $('#voucherCode').attr('voucher-code');
         var orderNotes = $('#orderNotes').val();
-        if(orderNotes == '' || orderNotes == null) {
+        if (orderNotes == '' || orderNotes == null) {
             orderNotes = 'Note';
         }
         var totalAmount = parseInt(document.getElementById('totalAmount').textContent.replace(/[,.]/g, ''));
@@ -308,7 +335,7 @@ $(document).ready(function () {
         }
         console.log(paymentDto);
 
-        if(typeOfPayment == 'whenReceive') {
+        if (typeOfPayment == 'whenReceive') {
             $.ajax({
                 type: "POST",
                 url: "/check-out/placeOrder-whenReceive",
@@ -323,7 +350,7 @@ $(document).ready(function () {
                 }
             })
         }
-        if(typeOfPayment == 'online') {
+        if (typeOfPayment == 'online') {
             $.ajax({
                 type: "POST",
                 url: "/check-out/placeOrder-online",
