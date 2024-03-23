bfb8424480d62501bf84246dd4c9c23daf0a2dfb
anhtuan2212 <lutan2212@gmail.com>
2024-03-23 16:29:30 +0700

    code bán hàng tại quầy

--- src/main/java/com/poly/BeeShoes/controller/CustomErrorController.java
+++ src/main/java/com/poly/BeeShoes/controller/CustomErrorController.java
@@ -10,19 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    @RequestMapping("/error")
+    @RequestMapping("/error/404")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
-
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
                 return "/error/404";
             }
         }
-        return "/error/error";
+        return "error/404";
     }
-
 }
 
--- src/main/resources/static/assets/cms/js/cashier/cashier.js
+++ src/main/resources/static/assets/cms/js/cashier/cashier.js
@@ -1,14 +1,18 @@
-function PrintOder() {
+function PrintBillOder() {
     let elementToPrint = document.getElementById("oder-print");
     printElement(elementToPrint);
 }
+
+resetData();
 
 function initSelect2(element) {
     $.HSCore.components.HSSelect2.init(element);
 }
 
+let canRunDetection = true;
 let dataShop = [];
 let dataProductDetails = [];
+let num_oder_add = null;
 window.onload = function () {
     // ToastSuccess('Tải hoàn tất !')
     $.ajax({
@@ -24,6 +28,191 @@ window.onload = function () {
         }
     })
 };
+
+// hàm thêm dấu chấm vào số
+function addCommasToNumber(number) {
+    let numberStr = number.toString().replace(/[^\d]/g, '');
+    let parts = [];
+    for (let i = numberStr.length, j = 0; i >= 0; i--, j++) {
+        parts.unshift(numberStr[i]);
+        if (j > 0 && j % 3 === 0 && i > 0) {
+            parts.unshift('.');
+        }
+    }
+    return parts.join('');
+}
+
+function PrintHtmlOder(product, numOder) {
+    let table = $(`#datatable_hd_${numOder}`);
+    let totalMoney = addCommasToNumber(Number(product.quantity) * Number(product.giaBan));
+    if (table.length > 0) {
+        let tbody = table.find('tbody');
+        if (table.find('.tr-show-no-data').length > 0) {
+            tbody.empty();
+        }
+        let tr = table.find(`#row_${numOder + '_' + product.id}`);
+        if (tr.length > 0) {
+            let curentQuantity = tr.find('.input-group-quantity-counter-control').val();
+            if (Number(curentQuantity) !== Number(product.quantity)) {
+                tr.find('.input-group-quantity-counter-control').val(product.quantity);
+                let total = Number(product.quantity) * Number(product.giaBan);
+                tr.find('.form-show-total-tr').val(addCommasToNumber(total));
+
+            }
+        } else {
+            let html = `
+                <tr id="row_${numOder + '_' + product.id}" data-id-product="${product.id}" data-id-hd="${numOder}">
+                    <td>
+                        <a class="media align-items-center" href="/cms/view-product?id=16">
+                            <img class="avatar avatar-lg mr-3" src="${product.anh}"
+                                 alt="Image Product">
+                            <div class="media-body">
+                                <h5 class="text-hover-primary mb-0">${product.ten}
+                                    (${product.tenMau + '-' + product.kichCo})</h5>
+                            </div>
+                        </a>
+                    </td>
+                    <td class="table-column-pl-0">
+                        <div class="js-quantity-counter input-group-quantity-counter">
+                            <input type="number"
+                                   class="js-result form-quantity form-control input-group-quantity-counter-control"
+                                   value="${product.quantity}">
+                            <div class="input-group-quantity-counter-toggle">
+                                <a class="js-minus input-group-quantity-counter-btn"
+                                   href="javascript:;">
+                                    <i class="tio-remove"></i>
+                                </a>
+                                <a class="js-plus input-group-quantity-counter-btn"
+                                   href="javascript:;">
+                                    <i class="tio-add"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="table-column-pl-0">
+                        <div class="input-group input-group-merge">
+                            <input type="text" class="form-control form-show-total-tr" value="${totalMoney}" readonly>
+                            <div class="input-group-prepend money-dv">
+                                <div class="input-group-text">VND</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="pl-1 pr-1">
+                        <a class="btn btn-white btn-delete-tr-oder" href="javascript:;">
+                            <i class="tio-delete-outlined"></i>
+                        </a>
+                    </td>
+                </tr>`;
+            tbody.append(html);
+        }
+    } else {
+        ToastError('Lỗi.')
+    }
+}
+
+function findProductByCode(code) {
+    if (!Array.isArray(dataProductDetails)) {
+        console.error("dataProductDetails không phải là một mảng.");
+        return null;
+    }
+    let data = [...dataProductDetails];
+    let product = data.find(pro => pro.maSanPham.toString().toLowerCase() === code.toString().toLowerCase());
+    if (product === undefined) {
+        ToastError('Mã sản phẩm không tồn tại.')
+        return null;
+    }
+    return product;
+}
+
+function findProductById(id) {
+    if (!Array.isArray(dataProductDetails)) {
+        console.error("dataProductDetails không phải là một mảng.");
+        return null;
+    }
+    let data = [...dataProductDetails];
+    let product = data.find(pro => pro.id === id);
+    return product;
+}
+
+function saveProductToOder(product, numOder, quantity) {
+    product.quantity = quantity;
+    let data = getListProductLocal(numOder);
+    let index = null;
+    let found = false;
+    let pro = null;
+    if (Number(product.soLuongTon) < 1) {
+        ToastError('Sản phẩm tạm hết hàng.');
+        return;
+    }
+    if (data !== null && Array.isArray(data)) {
+        for (let i = 0; i < data.length; i++) {
+            if (Number(data[i].id) === Number(product.id)) {
+                data[i].quantity = Number(data[i].quantity) + Number(quantity);
+                pro = data[i]
+                found = true;
+                index = i;
+                break;
+            }
+        }
+        if (!found) {
+            pro = product;
+            data.push(product);
+        }
+    } else {
+        data = []
+        pro = product;
+        data.push(product);
+    }
+    if (index !== null && Number(data[index].quantity) > Number(product.soLuongTon)) {
+        ToastError('Số lượng tồn kho đã tối đa.');
+        return;
+    }
+    if (pro !== null) {
+        PrintHtmlOder(pro, numOder)
+    }
+    saveToLocalStorage(data, numOder)
+    updateTotalMoney(numOder)
+}
+
+function resetData() {
+    localStorage.setItem('List_product_oder_1', JSON.stringify([]))
+    localStorage.setItem('List_product_oder_2', JSON.stringify([]))
+    localStorage.setItem('List_product_oder_3', JSON.stringify([]))
+    localStorage.setItem('List_product_oder_4', JSON.stringify([]))
+    localStorage.setItem('List_product_oder_5', JSON.stringify([]))
+
+}
+
+function saveToLocalStorage(listPro, oder) {
+    localStorage.setItem(`List_product_oder_${oder}`, JSON.stringify(listPro))
+}
+
+function getListProductLocal(oder) {
+    return JSON.parse(localStorage.getItem(`List_product_oder_${oder}`));
+}
+
+function updateTotalMoney(oder) {
+    let data = getListProductLocal(oder);
+    let total = 0;
+    if (data !== null && Array.isArray(data)) {
+        data.forEach((pro) => {
+            total += Number(pro.quantity) * Number(pro.giaBan);
+        })
+    }
+    let wrapper = $(`#oder_content_${oder}`);
+    let type = wrapper.find(`input[name="deliveryOptionCheckbox_hd_${oder}"]:checked`).val();
+    let shipingFee = 0;
+    if (type === 'CP') {
+        shipingFee = 30000;
+        if (total > 2000000) {
+            shipingFee = 0;
+        }
+    }
+    let payment = total + shipingFee;
+    wrapper.find('h4.total-money').text(addCommasToNumber(total) + 'đ')
+    wrapper.find('h4.shipping-money').text(addCommasToNumber(shipingFee) + 'đ')
+    wrapper.find('h3.payment-money').text(addCommasToNumber(payment) + 'đ')
+}
 
 function printElement(elementToPrint) {
     let iframe = document.getElementById("myFrame");
@@ -97,9 +286,123 @@ function Confirm(title, message, txt_cancel, txt_confirm) {
     })
 }
 
+function getOderNum(element) {
+    return $(element).closest('.oder-wraper-content').data('id-num-hd');
+}
+
+function deleteById(id, oder) {
+    let list = getListProductLocal(oder);
+    if (list === null) {
+        return;
+    }
+    list = list.filter(item => item.id !== id);
+    saveToLocalStorage(list, oder);
+    updateTotalMoney(oder);
+    ToastSuccess('Xóa thành công.')
+}
+
+let Detection = true;
+
+function addProductByBarcode(code) {
+    if (Detection) {
+        console.log(code)
+        let product = findProductByCode(code);
+        if (num_oder_add !== null && product !== null) {
+            saveProductToOder(product, num_oder_add, 1);
+        }
+        Detection = false;
+        setTimeout(() => {
+            Detection = true;
+        }, 2000);
+    }
+}
+
+function updateQuantityProduct(id, oder, operator, element) {
+    let data = getListProductLocal(oder);
+    let index = data.findIndex(item => Number(item.id) === Number(id));
+    if (index === -1) {
+        console.log('K tìm thấy dữ liệu.')
+        return;
+    }
+    if (data[index].quantity === 1 && operator === 'minus') {
+        let tr = $(element).closest('tr')
+        let tbody = tr.closest('tbody');
+        Confirm('Xác Nhận Xóa !', 'Thao tác không thể hoàn tác.', 'Hủy', 'Xác Nhận').then((check) => {
+            if (check) {
+                deleteById(id, oder);
+                if (tbody.find('tr').length === 1) {
+                    let html =
+                        `<tr class="tr-show-no-data">
+                    <td class="table-column-pr-0" rowspan="4">
+                        <h5 class="text-center">Chưa có dữ liệu.</h5>
+                    </td>
+                </tr>`;
+                    tbody.append(html)
+                }
+                tr.remove();
+            }
+        })
+    } else {
+        if (operator === 'minus') {
+            data[index].quantity = Number(data[index].quantity) - 1;
+        } else {
+            data[index].quantity = Number(data[index].quantity) + 1;
+        }
+        saveToLocalStorage(data, oder);
+        updateTotalMoney(oder);
+        PrintHtmlOder(data[index], oder);
+    }
+}
+
 $(document).on('ready', function () {
+
+    $(document).on('click', '.input-group-quantity-counter-btn', function () {
+        let oder = getOderNum(this);
+        let tr = $(this).closest('tr')
+        let id = tr.data('id-product')
+        if ($(this).hasClass('js-plus')) {
+            updateQuantityProduct(id, oder, 'plus', this)
+        } else {
+            updateQuantityProduct(id, oder, 'minus', this)
+        }
+    })
+    $(document).on('click', '.btn-delete-tr-oder', function () {
+        let tr = $(this).closest('tr');
+        let id = tr.data('id-product');
+        let oder = tr.data('id-hd');
+        let tbody = tr.closest('tbody');
+        Confirm('Xác Nhận Xóa !', 'Thao tác không thể hoàn tác.', 'Hủy', 'Xác Nhận').then((check) => {
+            if (check) {
+                deleteById(id, oder);
+                if (tbody.find('tr').length === 1) {
+                    let html =
+                        `<tr class="tr-show-no-data">
+                    <td class="table-column-pr-0" rowspan="4">
+                        <h5 class="text-center">Chưa có dữ liệu.</h5>
+                    </td>
+                </tr>`;
+                    tbody.append(html)
+                }
+                tr.remove();
+            }
+        })
+    })
+    $(document).on('change', '.option-select-receipt', function () {
+        let oder = getOderNum(this);
+        updateTotalMoney(oder);
+    })
+    $(document).on('click', '.li-item-ctsp-search', function () {
+        console.log('run')
+        let id_pro = $(this).data('ctsp-id');
+        let oder = getOderNum(this);
+        let product = findProductById(id_pro);
+        saveProductToOder(product, oder, 1);
+    })
+
     let modalBarcode = $('#form-modal-barcode');
     $(document).on('click', '.btn-scan-barcode', function () {
+        let oder = getOderNum(this);
+        num_oder_add = oder;
         modalBarcode.modal('show');
     })
     modalBarcode.on('shown.bs.modal', function () {
@@ -130,11 +433,9 @@ $(document).on('ready', function () {
                 Quagga.start();
             }
         );
-        let canRunDetection = true;
-
         Quagga.onDetected(function (result) {
             if (canRunDetection) {
-                ToastSuccess(result.codeResult.code);
+                addProductByBarcode(result.codeResult.code)
                 beepSound.play();
                 canRunDetection = false;
                 setTimeout(() => {
@@ -299,8 +600,17 @@ $(document).on('ready', function () {
     $(document).on('blur', '.form-input-search', function () {
         console.log('un focus')
         let formShow = $(this).closest('.wrapper-form-search').find('.div-form-search');
-        formShow.addClass('d-none')
+
+        // Kiểm tra xem chuột có nằm trong phạm vi của .div-form-search hay không
+        if (!formShow.is(':hover')) {
+            formShow.addClass('d-none');
+        }
     });
+
+    $(document).on('mouseleave', '.div-form-search', function () {
+        $(this).addClass('d-none');
+    });
+
     // INITIALIZATION OF ADD INPUT FILED
     // =======================================================
     $('.js-add-field').each(function () {
